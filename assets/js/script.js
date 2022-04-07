e2d8af3751f6105e320b00a13b790e3c42039f76
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-04-07 14:20:59 -0300

    applying-the-config

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,71 +2,110 @@
 const parrots = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
 
 function gameConfig() {
+    const gameConfig = {
+        playerName: document.getElementById('name').value ?? 'Desconhecido',
+        nCartas: document.getElementById('nCartas').value
+    }
 
+    gameConstructor(gameConfig);
+    game();
+
+    // sessionStorageConfig(gameConfig);
+}
+
+function sessionStorageConfig(gameConfig) {
+    sessionStorage.clear();
+    sessionStorage.setItem('config', JSON.stringify(gameConfig));
+}
+
+function gameConstructor(gameConfig) {
+    const parrotGame = document.getElementById('parrot-game');
+    const config = document.getElementById('config');
+    const nParrots = parrots.slice(0, gameConfig.nCartas);
+
+    nParrots.forEach(nParrot => {
+        parrotGame.innerHTML += `
+        <div class="parrot-card" data-parrots="${nParrot}">
+            <img src="./assets/img/${nParrot}.gif" alt="${nParrot}" class="front-face">
+            <img src="./assets/img/front.png" alt="Parrot Badge" class="back-face">
+        </div>
+        <div class="parrot-card" data-parrots="${nParrot}">
+            <img src="./assets/img/${nParrot}.gif" alt="${nParrot}" class="front-face">
+            <img src="./assets/img/front.png" alt="Parrot Badge" class="back-face">
+        </div>`;
+    })
+
+    parrotGame.style.display = 'grid';
+    parrotGame.style.gridTemplateColumns = `repeat(${gameConfig.nCartas}, auto)`;
+    config.style.display = 'none';
 }
 
 
 // Game functions and variables
-const cards = document.querySelectorAll('.memory-card');
+function game() {
+    const cards = document.querySelectorAll('.parrot-card');
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+    console.log(cards);
 
-function flipCard() {
-    if (lockBoard) {
-        return;
+    let hasFlippedCard = false;
+    let lockBoard = false;
+    let firstCard, secondCard;
+
+    function flipCard() {
+        if (lockBoard) {
+            return;
+        }
+
+        if (this === firstCard) {
+            return;
+        }
+
+        this.classList.toggle('flip');
+        if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+
+            return;
+        }
+
+        secondCard = this;
+
+        checkForMatch();
     }
 
-    if (this === firstCard) {
-        return;
+    function checkForMatch() {
+        const isMatch = firstCard.dataset.parrots === secondCard.dataset.parrots;
+        isMatch ? disableCards() : unFlipCards();
     }
 
-    this.classList.toggle('flip');
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-
-        return;
-    }
-
-    secondCard = this;
-
-    checkForMatch();
-}
-
-function checkForMatch() {
-    const isMatch = firstCard.dataset.parrots === secondCard.dataset.parrots;
-    isMatch ? disableCards() : unFlipCards();
-}
-
-function disableCards() {
-    firstCard.removeEventListener('click', flipCard);
-    secondCard.removeEventListener('click', flipCard);
-
-    resetBoard();
-}
-
-function unFlipCards() {
-    lockBoard = true;
-
-    setTimeout(() => {
-        firstCard.classList.remove('flip')
-        secondCard.classList.remove('flip')
+    function disableCards() {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
 
         resetBoard();
-    }, 1300);
+    }
+
+    function unFlipCards() {
+        lockBoard = true;
+
+        setTimeout(() => {
+            firstCard.classList.remove('flip')
+            secondCard.classList.remove('flip')
+
+            resetBoard();
+        }, 1300);
+    }
+
+    function resetBoard() {
+        [hasFlippedCard, lockBoard] = [false, false];
+        [firstCard, secondCard] = [null, null];
+    }
+
+    (function shuffle() {
+        cards.forEach(card => {
+            card.style.order = Math.floor((Math.random() * 12)).toString();
+        })
+    })();
+
+    cards.forEach(card => card.addEventListener('click', flipCard));
 }
-
-function resetBoard() {
-    [hasFlippedCard, lockBoard] = [false, false];
-    [firstCard, secondCard] = [null, null];
-}
-
-(function shuffle() {
-    cards.forEach(card => {
-        card.style.order = Math.floor((Math.random() * 12)).toString();
-    })
-})();
-
-cards.forEach(card => card.addEventListener('click', flipCard));
